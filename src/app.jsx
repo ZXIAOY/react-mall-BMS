e2523dc33b2b88ef5f870f506451e244851d2922
Zhangxiaoyu <zhangxiaoyu@apexsoft.com.cn>
2018-08-14 19:35:23 +0800

    完成webpack打包基础配置

---
+++ src/app.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+//引入字体文件
+import 'font-awesome/css/font-awesome.min.css';
+import './index.css';
+import './index.scss';
+
+ReactDOM.render(
+  <div>
+    <i className='fa fa-address-book'></i>
+    <h1>测试</h1>
+  </div>,
+  document.getElementById('app')
+);
