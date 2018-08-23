4bd372405ae6e1b09153071f5331764efcc40405
Zhangxiaoyu <zhangxiaoyu@apexsoft.com.cn>
2018-08-23 17:44:57 +0800

    完成基础框架的开发

--- src/page/home/index.jsx
+++ src/page/home/index.jsx
@@ -2,13 +2,19 @@
   home组件
 */
 import React from 'react';
+import PageTitle from 'component/page-title/index.jsx';
 
 class Home extends React.Component {
 
   render() {
     return (
       <div id='page-wrapper'>
-        <button className='btn btn-default'>测试</button>
+        <PageTitle title='首页'></PageTitle>
+        <div className="row">
+          <div className="col-md-12">
+            body
+          </div>
+        </div>
       </div>
     );
   }
