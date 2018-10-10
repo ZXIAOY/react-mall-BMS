be6912d9aa9a7ec66fe19791086fb536aa93e9e7
Zhangxiaoyu <zhangxiaoyu@apexsoft.com.cn>
2018-10-10 23:00:17 +0800

    用户-用户模块开发

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Link, Route, Redirect, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Link, Route, Redirect, Switch,} from 'react-router-dom';
+
+import Layout from 'component/layout/index.jsx';
 
 import Home from 'page/home/index.jsx';
 import Login from 'page/login/index.jsx';
-import Layout from 'component/layout/index.jsx';
+import User from 'page/user/index.jsx';
+import ErrorPage from 'page/error/index.jsx';
 
 class App extends React.Component {
   constructor(props) {
@@ -12,20 +15,24 @@ class App extends React.Component {
   }
 
   render() {
+    let layoutRouter = (
+      <Layout>
+        <Switch>
+          <Route exact path='/' component={Home} /> {/* exact表示严格匹配 */}
+          <Route path='/product' component={Home} /> 
+          <Route path='/product-category' component={Home} /> 
+          <Route path='/user/index' component={User} />
+          <Redirect exact from='/user' to='/user/index' />
+          <Route component={ErrorPage} /> 
+        </Switch>
+      </Layout>
+    );
+
     return (
       <Router>
         <Switch>
           <Route path='/login' component={Login}></Route>
-          <Route path='/' render={(props)=> (
-            <Layout>
-              <Switch>
-                <Route exact path='/' component={Home} /> {/* exact表示严格匹配 */}
-                <Route path='/product' component={Home} /> 
-                <Route path='/product-category' component={Home} /> 
-              </Switch>
-            </Layout>
-          )}>
-          </Route>
+          <Route path='/' render={(props)=> layoutRouter} />
         </Switch>
       </Router>
     );
